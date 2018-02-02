af5026aece1f6ab9df122d9954b4b2a70899c738
hanhpuu <35247425+hanhpuu@users.noreply.github.com>
2018-02-02 12:05:36 +0700

    Building Our First Route

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class PageController extends Controller 
+{
+public function about()
+    {
+	return "About Us Page";
+
+    }
+public function contact()
+    {
+	return "Contact Page";
+
+    }
+
+public function submitContact()
+    {
+	return "Submitted Contact";
+
+    }    
+}
